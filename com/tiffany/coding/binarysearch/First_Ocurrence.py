42f03352d4f47af0a1b1f378b050fe30c43b10f0
Tiffany Zhao <tiffanyzhaozx@gmail.com>
2020-04-10 14:51:10 -0700

    selection sort

---
+++ com/tiffany/coding/binarysearch/First_Ocurrence.py
@@ -0,0 +1,6 @@
+for i in range (0, 7):
+    for j in range(0, 7-i):
+        print(" ", end = " ")
+    for k in range (0, 2*i+1):
+        print("0", end = " ")
+    print(" ")
---
+++ com/tiffany/coding/sorting/Selection_Sort.py
@@ -0,0 +1,49 @@
+# Question: Given an array of integers, sort the elements in the array in ascending order.
+# The selection sort algorithm should be used to solve this problem.
+
+# Examples
+# {1} is sorted to {1}
+# {1, 2, 3} is sorted to {1, 2, 3}
+# {3, 2, 1} is sorted to {1, 2, 3}
+# {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+
+# Corner Cases
+# What if the given array is null? In this case, we do not need to do anything.
+# What if the given array is of length zero? In this case, we do not need to do anything.
+
+# Solution one: from max to min
+class Solution1():
+    def solve(self, array):
+        # step 1: for loop checking
+        # 1.1: checking from the end of the array to the beginning of the array
+        for i in range(len(array) - 1, 0, -1):
+            max = i
+            #1.2: nested loop, compare with item contained in the array to look for max#
+            for j in range(0, i+1): # does not include i + 1, end at i
+                if array[j] > array[max]:
+                    max = j
+            # step 2: swap
+            array[i], array[max] = array[max], array[i]
+
+        # step 3: print our sorted array
+        return array
+
+p1 = Solution1()
+array = [3,5,1,2,4,8]
+res = p1.solve(array)
+print(res)
+
+# Solution two: from min to max
+class Solution2():
+    def solve2(self, array):
+        for i in range(0, len(array), 1): # important
+            min = i
+            for j in range (i, len(array)): # important
+                if array[j] < array [min]:
+                    min = j
+            array[i], array[min] = array[min], array[i]
+        return array
+p2 = Solution2
+array = [3,5,1,2,4,8]
+res = p1.solve(array)
+print(res)
