afa4f96be33311b7346c1c9c25d6633714c19e4f
Tiffany Zhao <tiffanyzhaozx@gmail.com>
2020-04-16 21:44:53 -0700

    04162020

---
+++ com/tiffany/coding/Linked_List/Deisgn Doubly Linked List_v1.py
@@ -0,0 +1,8 @@
+class _ListNode(object):
+    def __init__(self, val):
+        # creating a value, a next pointer and a previous pointer
+        self.val = val
+        self.next = None
+        self.prev = None
+
+    def 
